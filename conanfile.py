a0dfaf5803e6b332ab7878535a6de0696cdc07c6
Ostapenko Alexey <dolu.bl@gmail.com>
2026-04-03 16:20:39 +0300

    [dto][generators] Корректировка перечня зависимостей.

--- conanfile.py
+++ conanfile.py
@@ -5,12 +5,5 @@ class FaradoConanFile(ConanFile):
     generators = "CMakeDeps", "CMakeToolchain"
 
     def requirements(self):
-        self.requires("zlib/1.2.11")
         self.requires("boost/1.83.0")
-        self.requires("libcurl/7.88.1")
-        self.requires("openssl/3.1.4")
-        self.requires("crowcpp-crow/1.0+5")
-        self.requires("sqlite3/3.42.0")
-        self.requires("cpr/1.10.5")
         self.requires("nlohmann_json/3.11.3")
-        self.requires("icu/74.2")
